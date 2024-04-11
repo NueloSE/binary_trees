b15b55270b4c2940faacc8d6d8b801b0b5a1f26a
Nuelo <akalo.emmanuel18@gmail.com>
2024-04-11 09:46:11 +0100

    fix: betty compliance check

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,17 +7,19 @@
  * Return: pointer to the new node on success. Otherwise NULL
 */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) {
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
 	binary_tree_t *new_node;
 
-	new_node =malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
-	if (parent == NULL){
+	if (parent == NULL)
+	{
 		new_node->parent = NULL;
 		new_node->n = value;
-		return new_node;
+		return (new_node);
 	}
 	new_node->parent = parent;
 	new_node->n = value;
