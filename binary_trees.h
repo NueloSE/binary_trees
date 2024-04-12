4be1be22f82f25961daa03dc33405d4e46c1e698
Nuelo <akalo.emmanuel18@gmail.com>
2024-04-12 10:02:38 +0100

    feat: implement delete feature

--- binary_trees.h
+++ binary_trees.h
@@ -29,6 +29,7 @@ typedef struct binary_tree_s binary_tree_t;
 
 /*******************PRINTERS*********************/
 void binary_tree_print(const binary_tree_t *);
+void print_num(int number);
 
 /************************PROTOTYPES****************************/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
