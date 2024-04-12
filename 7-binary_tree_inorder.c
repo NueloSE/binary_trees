960a1e820a290122eb6b608133495c2573d03279
Nuelo <akalo.emmanuel18@gmail.com>
2024-04-12 10:56:52 +0100

    feat: implement in-order traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * print_num - Prints a number
+ *
+ * @number: Number to be printed
+ */
+void print_num(int number)
+{
+	printf("%d\n", number);
+}
+
+/**
+ * binary_tree_inorder - goes through a binary tree using pre-order traversal
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ * the value in the node must be passed a a parameter to this function
+ * Return: always void
+*/
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
